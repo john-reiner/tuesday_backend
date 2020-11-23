652b14a65b1e034ac7636ac1b58ca882eeddecf7
john-reiner <johnreiner1990@gmail.com>
2020-11-23 15:18:30 -0500

    added logout and logged_in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
-class SessionsController < ApplicaionController
+class SessionsController < ApplicationController
+    include CurrentUserConcern
     def create 
         user = User.find_by(email: params["user"]["email"])
         .try(:authenticate, params["user"]["password"])
@@ -13,5 +14,22 @@ class SessionsController < ApplicaionController
         else 
             render json: { status: 401 }
         end
+
+        def logged_in
+            if @current_user 
+                render json: {
+                    logged_in: true,
+                    user: @current_user
+                }
+            else 
+                render json: {
+                    logged_in: false
+                }
+            end 
+        end
+        def logout 
+            reset_session
+            render json: { status: 200, logged_out: true }
+        end 
     end 
 end 
